687b3380bc46c5536e231a3a2c3ea1e4c1156108
freek <freek@spatie.be>
2016-02-21 21:02:42 +0100

    remove php7 syntax

--- src/Exceptions/CannotStartDump.php
+++ src/Exceptions/CannotStartDump.php
@@ -6,7 +6,12 @@ use Exception;
 
 class CannotStartDump extends Exception
 {
-    public static function emptyParameter(string $name) : CannotStartDump
+    /**
+     * @param string $name
+     *
+     * @return \Spatie\DbDumper\Exceptions\CannotStartDump
+     */
+    public static function emptyParameter($name)
     {
         return new static("Parameter {$name} cannot be empty.");
     }
--- src/Exceptions/DumpFailed.php
+++ src/Exceptions/DumpFailed.php
@@ -7,17 +7,28 @@ use Symfony\Component\Process\Process;
 
 class DumpFailed extends Exception
 {
-    public static function processDidNotEndSuccessfully(Process $process) : DumpFailed
+    /**
+     * @param \Symfony\Component\Process\Process $process
+     *
+     * @return \Spatie\DbDumper\Exceptions\DumpFailed
+     */
+    public static function processDidNotEndSuccessfully(Process $process)
     {
         return new static("The dump process failed with exitcode {$process->getExitCode()} : {$process->getExitCodeText()}");
     }
 
-    public static function dumpfileWasNotCreated() : DumpFailed
+    /**
+     * @return \Spatie\DbDumper\Exceptions\DumpFailed
+     */
+    public static function dumpfileWasNotCreated()
     {
         return new static('The dumpfile could not be created');
     }
 
-    public static function dumpfileWasEmpty() : DumpFailed
+    /**
+     * @return \Spatie\DbDumper\Exceptions\DumpFailed
+     */
+    public static function dumpfileWasEmpty()
     {
         return new static('The created dumpfile is empty');
     }
