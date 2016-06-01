f2357a67cd2f2d4ae647f49582ba4b95cbd41054
freek <freek@spatie.be>
2016-06-01 22:47:37 +0200

    nitpick

--- src/Exceptions/CannotSetParameter.php
+++ src/Exceptions/CannotSetParameter.php
@@ -14,6 +14,6 @@ class CannotSetParameter extends Exception
      */
     public static function conflictingParameters($name, $conflictName)
     {
-        return new static("Cannot set {$name}! Conflicts with parameter {$conflictName}.");
+        return new static("Cannot set {$name} because it conflicts with parameter {$conflictName}.");
     }
 }
