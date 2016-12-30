46cc0321f508ec47f36decc243ba4e7c9d537055
Valentin Ivaşcu <oriceon@gmail.com>
2016-12-30 09:09:15 +0200

    Update MySql.php

--- src/Databases/MySql.php
+++ src/Databases/MySql.php
@@ -122,7 +122,7 @@ class MySql extends DbDumper
     public function getDumpCommand(string $dumpFile, string $temporaryCredentialsFile): string
     {
         $quote = (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' ? '"' : "'");
-        
+
         $command = [
             "{$quote}{$this->dumpBinaryPath}mysqldump{$quote}",
             "--defaults-extra-file=\"{$temporaryCredentialsFile}\"",
