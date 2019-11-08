0ff13b03c05dd193c096148b0a3423a11e052ea9
Colyn <colyn.brown@gmail.com>
2019-11-08 15:32:32 -0700

    Fixed formatting

--- src/Databases/PostgreSql.php
+++ src/Databases/PostgreSql.php
@@ -70,7 +70,7 @@ class PostgreSql extends DbDumper
         $command = [
             "{$quote}{$this->dumpBinaryPath}pg_dump{$quote}",
             "-U {$this->userName}",
-            '-h ' . ($this->socket === '' ? $this->host : $this->socket),
+            '-h '.($this->socket === '' ? $this->host : $this->socket),
             "-p {$this->port}",
         ];
 
@@ -78,7 +78,7 @@ class PostgreSql extends DbDumper
             $command[] = '--inserts';
         }
 
-        if (!$this->createTables) {
+        if (! $this->createTables) {
             $command[] = '--data-only';
         }
 
@@ -86,12 +86,12 @@ class PostgreSql extends DbDumper
             $command[] = $extraOption;
         }
 
-        if (!empty($this->includeTables)) {
-            $command[] = '-t ' . implode(' -t ', $this->includeTables);
+        if (! empty($this->includeTables)) {
+            $command[] = '-t '.implode(' -t ', $this->includeTables);
         }
 
-        if (!empty($this->excludeTables)) {
-            $command[] = '-T ' . implode(' -T ', $this->excludeTables);
+        if (! empty($this->excludeTables)) {
+            $command[] = '-T '.implode(' -T ', $this->excludeTables);
         }
 
         return $this->echoToFile(implode(' ', $command), $dumpFile);
