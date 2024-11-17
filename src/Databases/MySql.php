71632c44af4433bec5a5f9e500109310cfabaaad
Peter Temi <info@diadal.com.ng>
2024-11-17 06:26:16 +0100

    Update MySql.php
    set default value `setSkipSsl` to `false`

--- src/Databases/MySql.php
+++ src/Databases/MySql.php
@@ -44,7 +44,7 @@ class MySql extends DbDumper
         $this->port = 3306;
     }
 
-    public function setSkipSsl(bool $skipSsl): self
+    public function setSkipSsl(bool $skipSsl = false): self
     {
         $this->skipSsl = $skipSsl;
 
