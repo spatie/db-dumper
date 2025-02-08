56bc54c9c2c38376026dbb5512f65b96e9236d04
Nathan Byloff <nbyloff@users.noreply.github.com>
2025-02-08 12:36:28 -0600

    mariadb-dump

--- src/Databases/MySql.php
+++ src/Databases/MySql.php
@@ -207,7 +207,12 @@ class MySql extends DbDumper
             "{$quote}{$this->dumpBinaryPath}mysqldump{$quote}",
             "--defaults-extra-file=\"{$temporaryCredentialsFile}\"",
         ];
+        $finalDumpCommand = $this->getCommonDumpCommand($command);
+        return $this->echoToFile($finalDumpCommand, $dumpFile);
+    }
 
+    public function getCommonDumpCommand(array $command): string
+    {
         if (! $this->createTables) {
             $command[] = '--no-create-info';
         }
@@ -278,7 +283,7 @@ class MySql extends DbDumper
             $finalDumpCommand .= " | {$sedCommand}";
         }
 
-        return $this->echoToFile($finalDumpCommand, $dumpFile);
+        return $finalDumpCommand;
     }
 
     public function getContentsOfCredentialsFile(): string
